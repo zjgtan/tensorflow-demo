b5a0984b3caaa03f42609eed01b12a079e918b9e
zjgtan <zjgtan@126.com>
2019-03-31 10:33:40 +0800

    session

---
+++ src/tf_session.py
@@ -0,0 +1,11 @@
+# coding: utf8
+import tensorflow as tf
+
+m1 = tf.constant([[1, 3]])
+m2 = tf.constant([[2], [3]])
+
+product = tf.matmul(m1, m2)
+
+with tf.Session() as sess:
+    result = sess.run(product)
+    print result
