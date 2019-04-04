c1784a465c22a9aee17fbfec5d256ca2aeb02f53
zjgtan <zjgtan@126.com>
2019-04-04 17:27:20 +0800

    mlp

--- src/tf_mnist_mlp.py
+++ src/tf_mnist_mlp.py
@@ -10,31 +10,29 @@ n_batch = mnist.train.num_examples / batch_size
 x = tf.placeholder(tf.float32, [None, 784])
 y = tf.placeholder(tf.float32, [None, 10])
 
-W1 = tf.Variable(tf.random.normal([784, 500], stddev=0.1))
-b1 = tf.Variable(tf.random.normal([500], stddev=0.1))
-o1 = tf.nn.tanh(tf.matmul(x, W1) + b1)
+dense1 = tf.layers.dense(
+        inputs = x, 
+        units = 500, 
+        activation = tf.nn.tanh,
+        use_bias = True,
+        kernel_regularizer = tf.contrib.layers.l2_regularizer(0.003))
 
-W2 = tf.Variable(tf.random.normal([500, 500], stddev=0.1))
-b2 = tf.Variable(tf.random.normal([500], stddev=0.1))
-o2 = tf.nn.tanh(tf.matmul(o1, W2) + b2)
+dense2 = tf.layers.dense(
+        inputs = dense1, 
+        units = 500, 
+        activation = tf.nn.tanh,
+        use_bias = True,
+        kernel_regularizer = tf.contrib.layers.l2_regularizer(0.003))
 
+prediction = tf.layers.dense(
+        inputs = dense2, 
+        units = 10, 
+        activation = tf.nn.softmax,
+        kernel_regularizer = tf.contrib.layers.l2_regularizer(0.003))
 
-
-W3 = tf.Variable(tf.random.normal([500, 10], stddev=0.1))
-b3 = tf.Variable(tf.random.normal([10], stddev=0.1))
-prediction = tf.nn.softmax(tf.matmul(o2, W3) + b3)
-
-# L2正则
-regularizer = tf.contrib.layers.l2_regularizer(0.001)
-regularization = regularizer(W1) + regularizer(W2) + regularizer(b1) + regularizer(b2)
-
-#loss = tf.reduce_mean(tf.square(y - prediction))
-#loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = y, logits = prediction)) + \
-#        regularization
 
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = y, logits = prediction))
 
-#train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
 train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
 
 init = tf.global_variables_initializer()
@@ -49,6 +47,7 @@ with tf.Session() as sess:
     for epoch in range(100):
         for batch in range(n_batch):
             batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+            print type(batch_xs)
             sess.run(train_step, feed_dict = {x: batch_xs, y: batch_ys})
 
         acc = sess.run(accuracy, feed_dict = {x: mnist.test.images, y: mnist.test.labels})
